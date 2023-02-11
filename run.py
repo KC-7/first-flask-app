c0ff6c7ed0e611441c149c3e7c25a16f99072f2d
Kieran Collins <kiecol@protonmail.com>
2023-02-11 17:39:01 +0000

    Set up and route about and contact pages

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
 import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html") # Flask expects this file to be in templates folder
+    return render_template("index.html") # Note: Flask expects this file to be in templates folder
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html") 
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html") 
+
 
 if __name__ == "__main__":
     app.run(
